cb25b627cb2b33c7c231729ec9c17f84e80aea31
danilotiago <programacao.desenvolvimento@gmail.com>
2021-05-27 15:14:37 -0300

    initial commit

---
+++ src/main/java/app/projetaria/codeflixvideocatalog/CodeflixVideoCatalogApplication.java
@@ -0,0 +1,13 @@
+package app.projetaria.codeflixvideocatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeflixVideoCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeflixVideoCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/projetaria/codeflixvideocatalog/CodeflixVideoCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package app.projetaria.codeflixvideocatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeflixVideoCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
